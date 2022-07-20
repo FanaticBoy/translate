caaf81aadc2d13e96f4821619617dcde02bacd14
FanaticBoy <98470105+FanaticBoy@users.noreply.github.com>
2022-07-20 20:57:42 +0800

    Update db_conn.py

--- db/db_conn.py
+++ db/db_conn.py
@@ -1,6 +1,6 @@
 from pymysql import Connect
 class Connect_sql:
     def __init__(self):
-        self.conn = Connect(host='sh-cynosdbmysql-grp-78qgbl24.sql.tencentcdb.com', port=21087, user='root', passwd='ZHLsk153150274@', db='users')
+        self.conn = Connect(host='自己数据库主机', port=数据库端口, user='root', passwd='自己密码', db='users')
 if __name__ == '__main__':
     sql = Connect_sql()
